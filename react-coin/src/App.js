e474c16d2ce35a126c48ff8e5488481a0d7b234a
yoonjinlee <yoonjinifer@gmail.com>
2023-01-09 00:27:48 +0900

    add: bitcoinlist 실습

---
+++ react-coin/src/App.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => { 
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then(response => response.json()
+        .then((json) => { 
+          setCoins(json);
+          setLoading(false);
+        }
+    ))
+  }, [])
+  return (
+    <div>
+      <h1>The coins! {loading ? "": `(${coins.length})`}</h1>
+      {loading ? <strong>Loading...</strong> :<ul>
+        {coins.map((coin, index) => <li key={index}>{coin.name} ({coin.symbol}) : ${ coin.quotes.USD.price}</li>)}
+      </ul>}
+      
+    </div>
+  );
+}
+
+export default App;
