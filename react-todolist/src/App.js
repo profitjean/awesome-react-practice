845835661117480d7cce289345650e2e34d45717
yoonjinlee <yoonjinifer@gmail.com>
2023-01-09 00:01:50 +0900

    add: todolist 실습

---
+++ react-todolist/src/App.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => { 
+    event.preventDefault();
+    console.log(toDo);
+    if (toDo === "") { 
+      return;
+    }
+    
+    setToDos((currentArray) => [toDo, ...currentArray])
+    setToDo("");
+  }
+  console.log(toDos);
+  return <div>
+    <h1>My To Dos [{ toDos.length}]</h1>
+    <form onSubmit={ onSubmit}>
+      <input onChange={onChange} value={toDo} type="text" placeholder="write your to do" />
+      <button>add to do</button>
+    </form>
+    <hr />
+    <ul>
+      {toDos.map((item,index) => <li key={index}>{ item}</li>)}
+    </ul>
+    
+  </div>
+}
+
+export default App;
