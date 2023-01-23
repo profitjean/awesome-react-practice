87785ce9b81d56706be3b777a0c5784ee5e6db1e
yoonjinlee <yoonjinifer@gmail.com>
2023-01-24 03:15:07 +0900

    Update: CSS 라이브러리 적용 예제

---
+++ react-todo-app/src/components/Lists.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export default function List({ todoData, setTodoData }) {
+  const handleCompleteChange = (id) => {
+    let newTodoData = todoData.map((data) => {
+      if (data.id === id) {
+        data.completed = !data.completed;
+      }
+      return data;
+    });
+    console.log('test', newTodoData);
+    setTodoData(newTodoData);
+  };
+
+  const handleClick = (id) => {
+    let newTodoData = todoData.filter((data) => data.id !== id);
+
+    setTodoData(newTodoData);
+    console.log('newTodoData', newTodoData);
+  };
+
+  return (
+    <div>
+      {todoData.map((data) => (
+        <div key={data.id}>
+          <div
+            className={`flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}>
+            <div className="items-center">
+              <input
+                type="checkbox"
+                onChange={() => handleCompleteChange(data.id)}
+                defaultChecked={false}
+              />{' '}
+              <span className={data.completed && 'line-through'}>{data.title}</span>
+            </div>
+            <div className="items-center">
+              <button onClick={() => handleClick(data.id)}>x</button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
