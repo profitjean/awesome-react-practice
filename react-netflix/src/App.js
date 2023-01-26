12fb3aa180ebe7ad7656658d97307280b939bde7
yoonjinlee <yoonjinifer@gmail.com>
2023-01-27 01:46:05 +0900

    Update: row component

--- react-netflix/src/App.js
+++ react-netflix/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Nav from './components/Nav';
 import Banner from './components/Banner';
+import Row from './components/Row';
+import requests from './api/request';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Banner />
+      <Row title="NETFLIX ORIGINALS" id="NO" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} isLargeRow />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies} />
     </div>
   );
 }
